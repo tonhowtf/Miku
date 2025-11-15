9c13db8b5bca4ac863db7950f9f17e3bd5310fb0
Tonho <tonhowtf@gmail.com>
2025-11-15 17:45:53 -0300

    feat: add run.py to initialize Django and run bot in a separate thread

---
+++ run.py
@@ -0,0 +1,23 @@
+import os
+import django
+from threading import Thread
+from dotenv import load_dotenv
+
+load_dotenv()
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+django.setup()
+
+def run_bot():
+    from bot.bot import main
+    main()
+
+def run_django():
+    from django.core.management import execute_from_command_line
+    execute_from_command_line(['manage.py', 'runserver', '0.0.0.0:80'])
+
+if __name__ == '__main__':
+    bot_thread = Thread(target=run_bot, daemon=True)
+    bot_thread.start()
+    
+    run_django()
