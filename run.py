3307fdce8ab2301b6bba9d6454341d95e1a22383
Tonho <tonhowtf@gmail.com>
2025-11-15 20:08:43 -0300

    AAAAAAAAAAAAAAA

--- run.py
+++ run.py
@@ -7,32 +7,31 @@ import time
 import sys
 
 
-subprocess.run(['pip', 'install', '-r', 'requirements.txt'], check=True)
-
-
 load_dotenv()
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
 django.setup()
 
 from django.core.management import call_command
+
+subprocess.run(['pip', 'install', '-r', 'requirements.txt'], check=True)
+
+call_command('collectstatic', '--noinput')
+
 call_command('migrate', '--noinput')
 
-subprocess.run([sys.executable, '-m', 'django', 'collectstatic', '--noinput'])
 subprocess.run([sys.executable, 'create_user.py'])
 
 def run_django():
     from django.core.management import execute_from_command_line
-    execute_from_command_line(["manage.py", "runserver", "0.0.0.0:80", "--noreload"])
+    execute_from_command_line(['manage.py', 'runserver', '0.0.0.0:80', '--noreload'])
 
-def run_bot():
-    from bot.bot import main
-    main()
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     django_thread = Thread(target=run_django, daemon=True)
     django_thread.start()
 
     time.sleep(3)
 
-    run_bot()
+    from bot.bot import main
+    main()
