e5037d619cbbaef481fd20e2646d7ff89585ece5
Tonho <tonhowtf@gmail.com>
2025-11-15 13:22:53 -0300

    register Profile and StoryPersistance models in Django admin with custom configurations

--- bot/admin.py
+++ bot/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Profile, StoryPersistance
 
-# Register your models here.
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['username', 'active']
+    list_filter = ['active']
+
+@admin.register(StoryPersistance)
+class StoryPersistanceAdmin(admin.ModelAdmin):
+    list_display = ['url', 'profile', 'downloaded_at']
+    list_filter = ['profile', 'downloaded_at']
+    readonly_fields = ['downloaded_at']
