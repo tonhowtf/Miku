ad421d9e3ff53cebb6d529596cdc323c3f7ea098
Tonho <tonhowtf@gmail.com>
2025-11-15 19:54:23 -0300

    fix: set DEBUG to False for production readiness

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d^66=)d)@lu2i79dmpie!&%2ij4-o)!_w@hd99awkc)3u=441w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
